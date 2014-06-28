076b51c7c6936933cc7ce045460a299a6c23fdb8
BartoszJanota <bartosz.janota@gmail.com>
2014-06-28 17:25:22 +0200

    method_it_item section

--- src/sections/method_id_item_section.py
+++ src/sections/method_id_item_section.py
@@ -4,6 +4,7 @@ Created on 5 cze 2014
 @author: Przemek
 '''
 from src.parser.measurable import Measurable
+from src.items.method_id_item import MethodIdItem
 
 
 class MethodIdItemSection(Measurable):
@@ -17,3 +18,21 @@ class MethodIdItemSection(Measurable):
         Constructor
         '''
         Measurable.__init__(self, parent)
+        
+        method_id_item = MethodIdItem(self)
+        method_id_item.classIdx = 0
+        method_id_item.protoIdx = 0
+        method_id_item.nameIdx = 0
+        self.data.append(method_id_item)
+        
+        method_id_item = MethodIdItem(self)
+        method_id_item.classIdx = 0
+        method_id_item.protoIdx = 1
+        method_id_item.nameIdx = 7
+        self.data.append(method_id_item)
+        
+        method_id_item = MethodIdItem(self)
+        method_id_item.classIdx = 1
+        method_id_item.protoIdx = 0
+        method_id_item.nameIdx = 0
+        self.data.append(method_id_item)
