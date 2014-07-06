6d9fe0c52175477be2d86378d5e1f99fa77b83a6
BartoszJanota <bartosz.janota@gmail.com>
2014-07-06 16:09:57 +0200

    addTypeItem function fixed

--- src/sections/type_id_item_section.py
+++ src/sections/type_id_item_section.py
@@ -20,31 +20,10 @@ class TypeIdItemSection(Measurable):
         Measurable.__init__(self, parent)
 
     def initialize(self):
-        #item = TypeIdItem(self)
-        # item.data.value = 1
-        #item.data.ref_type = "index"
-        #item.data.ref = self.getRoot().string_id_item_section.data[1]
-        #self.data.append(item)
-        #item = TypeIdItem(self)
-        # item.data.value = 2
-        #item.data.ref_type = "index"
-        #item.data.ref = self.getRoot().string_id_item_section.data[2]
-        #self.data.append(item)
-        #item = TypeIdItem(self)
-        # item.data.value = 4
-        #item.data.ref_type = "index"
-        #item.data.ref = self.getRoot().string_id_item_section.data[4]
-        #self.data.append(item)
-        #item = TypeIdItem(self)
-        # item.data.value = 6
-        #item.data.ref_type = "index"
-        #item.data.ref = self.getRoot().string_id_item_section.data[6]
-        #self.data.append(item)
-        
         self.addTypeIdItemAndSort("LMain;");
         self.addTypeIdItemAndSort("Ljava/lang/Object;");
+        self.addTypeIdItemAndSort("[Ljava/lang/String;");#this one should be added as a fourth one, different order only for testing
         self.addTypeIdItemAndSort("V");
-        self.addTypeIdItemAndSort("[Ljava/lang/String;");
         
     def addTypeIdItemAndSort(self, typeStringName):
         string_id = 0
@@ -58,6 +37,9 @@ class TypeIdItemSection(Measurable):
         self.data.append(item)
         
         
-        #if len(self.data):
-        #    self.data.sort(cmp=None, key=lambda x: x.getStringIndex(), reverse=False)
+        if len(self.data):
+            self.data.sort(cmp=None, key=lambda x: self.getRoot().string_id_item_section.getItemIndex(x.data.ref), reverse=False)
         
+        #print "addTypeIdItemAndSort:"
+        #for x in self.data:
+        #    print self.getRoot().string_id_item_section.getItemIndex(x.data.ref)
