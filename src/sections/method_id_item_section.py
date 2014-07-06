6bfe59b08d68a202f3fb85b375939a8e2f00c147
PrzemekBurczyk <przemek.burczyk@gmail.com>
2014-07-06 18:22:51 +0200

    Implemented methodId add

--- src/sections/method_id_item_section.py
+++ src/sections/method_id_item_section.py
@@ -19,39 +19,34 @@ class MethodIdItemSection(Measurable):
         '''
         Measurable.__init__(self, parent)
 
+    def add(self, class_idx, proto_idx, name_idx):
+        method_id_item = MethodIdItem(self)
+        method_id_item.classIdx.ref_type = "index"
+        method_id_item.classIdx.ref = class_idx
+        method_id_item.protoIdx.ref_type = "index"
+        method_id_item.protoIdx.ref = proto_idx
+        method_id_item.nameIdx.ref_type = "index"
+        method_id_item.nameIdx.ref = name_idx
+
+        currentClassIdx = class_idx.parent.getItemIndex(class_idx)
+        currentProtoIdx = proto_idx.parent.getItemIndex(proto_idx)
+        currentNameIdx = name_idx.parent.getItemIndex(name_idx)
+
+        for item in self.data:
+            existingClassIdx = item.classIdx.ref.parent.getItemIndex(item.classIdx.ref)
+            existingProtoIdx = item.protoIdx.ref.parent.getItemIndex(item.protoIdx.ref)
+            existingNameIdx = item.nameIdx.ref.parent.getItemIndex(item.nameIdx.ref)
+
+            if currentClassIdx == existingClassIdx and currentProtoIdx == existingProtoIdx and currentNameIdx == existingNameIdx:
+                return item
+
+        self.data.append(method_id_item)
+        self.data.sort(cmp=None, key=lambda x: (x.classIdx.ref.parent.getItemIndex(x.classIdx.ref),
+                                                x.nameIdx.ref.parent.getItemIndex(x.nameIdx.ref),
+                                                x.protoIdx.ref.parent.getItemIndex(x.protoIdx.ref)), reverse=False)
+        return method_id_item
+
     def initialize(self):
-        method_id_item = MethodIdItem(self)
-        # method_id_item.classIdx.value = 0
-        method_id_item.classIdx.ref_type = "index"
-        method_id_item.classIdx.ref = self.getRoot().type_id_item_section.data[0]
-        # method_id_item.protoIdx.value = 0
-        method_id_item.protoIdx.ref_type = "index"
-        method_id_item.protoIdx.ref = self.getRoot().proto_id_item_section.data[0]
-        # method_id_item.nameIdx.value = 0
-        method_id_item.nameIdx.ref_type = "index"
-        method_id_item.nameIdx.ref = self.getRoot().string_id_item_section.data[0]
-        self.data.append(method_id_item)
-        
-        method_id_item = MethodIdItem(self)
-        # method_id_item.classIdx.value = 0
-        method_id_item.classIdx.ref_type = "index"
-        method_id_item.classIdx.ref = self.getRoot().type_id_item_section.data[0]
-        # method_id_item.protoIdx.value = 1
-        method_id_item.protoIdx.ref_type = "index"
-        method_id_item.protoIdx.ref = self.getRoot().proto_id_item_section.data[1]
-        # method_id_item.nameIdx.value = 7
-        method_id_item.nameIdx.ref_type = "index"
-        method_id_item.nameIdx.ref = self.getRoot().string_id_item_section.data[7]
-        self.data.append(method_id_item)
-        
-        method_id_item = MethodIdItem(self)
-        # method_id_item.classIdx.value = 1
-        method_id_item.classIdx.ref_type = "index"
-        method_id_item.classIdx.ref = self.getRoot().type_id_item_section.data[1]
-        # method_id_item.protoIdx.value = 0
-        method_id_item.protoIdx.ref_type = "index"
-        method_id_item.protoIdx.ref = self.getRoot().proto_id_item_section.data[0]
-        # method_id_item.nameIdx.value = 0
-        method_id_item.nameIdx.ref_type = "index"
-        method_id_item.nameIdx.ref = self.getRoot().string_id_item_section.data[0]
-        self.data.append(method_id_item)
+        self.add(self.getRoot().type_id_item_section.data[0], self.getRoot().proto_id_item_section.data[0], self.getRoot().string_id_item_section.data[0])
+        self.add(self.getRoot().type_id_item_section.data[0], self.getRoot().proto_id_item_section.data[1], self.getRoot().string_id_item_section.data[7])
+        self.add(self.getRoot().type_id_item_section.data[1], self.getRoot().proto_id_item_section.data[0], self.getRoot().string_id_item_section.data[0])
